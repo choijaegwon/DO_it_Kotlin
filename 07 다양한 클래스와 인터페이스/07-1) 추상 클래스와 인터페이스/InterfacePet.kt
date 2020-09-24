9c3c7f6b1ddc86f0fcd200f9b41b898e59b825da
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-09-24 16:24:00 +0900

    Pet 인터페이스에 species 프로퍼티 추가하기

--- 07 다양한 클래스와 인터페이스/07-1) 추상 클래스와 인터페이스/InterfacePet.kt
+++ 07 다양한 클래스와 인터페이스/07-1) 추상 클래스와 인터페이스/InterfacePet.kt
@@ -1,7 +1,11 @@
 package chap07.section1
 
 interface Pet {
-    var category: String // abstract 키워드가 없어도 기본은 추상 프로퍼티
+    var category: String
+    val msgTags: String
+        get() = "I'm your lovely pet!"
+
+    var species: String // 종을 위한 프로퍼티
     fun feeding() // 마찬가지로 추상 메서드
     fun patting() { // 일반 메서드: 구현부를 포함하면 일반적인 메서드로 기본이 됨
         println("Keep patting!") // 구현부
@@ -9,6 +13,7 @@ interface Pet {
 }
 
 class Cat(name: String, override var category: String) : Pet, Animal(name){
+    override var species: String = "cat" //프로퍼티를 오버라딩해 종을 특정
     override fun feeding(){
         println("Feed the cat a tuna can!")
     }
