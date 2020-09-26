c6ad5574f96058e636fbe8f8e1ce712df94df9ec
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-09-26 22:32:56 +0900

    컬렉션의 요소 집계 확장 함수 사용하기-요소의 개수 반환하기

---
+++ 09 컬렉션/09-4) 컬렉션의 확장 함수/ExtensionCount3.kt
@@ -0,0 +1,11 @@
+package chap09.section4
+
+fun main(){
+    val list = listOf(1, 2, 3, 4, 5, 6)
+    val listPair = listOf(Pair("A", 300), Pair("B", 200), Pair("C", 100))
+    val map = mapOf(11 to "Java", 22 to "Kotlin", 33 to "C++")
+
+    // count: 조건에 맞는 요소 개수 반환
+    println(list.count{it % 2 == 0})
+
+}
