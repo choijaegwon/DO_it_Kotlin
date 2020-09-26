781e4f60dc61e34e349a3597ab20a45b0b235a42
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-09-26 22:40:54 +0900

    컬렉션의 요소 집계 확장 함수 사용하기-모든 요소 합산하기

---
+++ 09 컬렉션/09-4) 컬렉션의 확장 함수/ExtensionCount6.kt
@@ -0,0 +1,10 @@
+package chap09.section4
+
+fun main(){
+    val list = listOf(1, 2, 3, 4, 5, 6)
+    val listPair = listOf(Pair("A", 300), Pair("B", 200), Pair("C", 100))
+    val map = mapOf(11 to "Java", 22 to "Kotlin", 33 to "C++")
+
+    // sumBy: 식에 의해 도출된 모든 요소를 합산
+    println(listPair.sumBy { it.second })
+}
