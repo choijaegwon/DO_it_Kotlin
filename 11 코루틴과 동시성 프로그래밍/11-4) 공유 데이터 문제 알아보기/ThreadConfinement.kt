b9226c3531cd4b5ce563b5929bca878eefd8db71
choijaegwon <68246962+choijaegwon@users.noreply.github.com>
2020-09-28 21:29:02 +0900

    스레드에 가두기

---
+++ 11 코루틴과 동시성 프로그래밍/11-4) 공유 데이터 문제 알아보기/ThreadConfinement.kt
@@ -0,0 +1,32 @@
+package com.acaroom.kotlin.chap04.section04.confinement
+
+import chap11.section4.massiveRun
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.system.measureTimeMillis
+// 단일 스레드 문맥을 선언한다
+val counterContext = newSingleThreadContext("CounterContext")
+var counter = 0
+
+suspend fun massiveRun(context: CoroutineContext, action: suspend () -> Unit) {
+    val n = 1000
+    val k = 1000
+    val time = measureTimeMillis {
+        val jobs = List(n) {
+            GlobalScope.launch(context) {
+                repeat(k) { action() }
+            }
+        }
+        jobs.forEach { it.join() }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
+
+fun main() = runBlocking<Unit> {
+    massiveRun {
+        withContext(counterContext){ // 단일 스레드에 가둠
+            counter++
+        }
+    }
+    println("Counter = $counter")
+}
